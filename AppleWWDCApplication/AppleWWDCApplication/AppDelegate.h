c8f08cb3bef2d926523823fe7cb187dc25fc75c4
jiangyiwei <jiangyiweijw@163.com>
2016-07-29 16:25:07 +0800

    创建项目，首次上传仓库

---
+++ AppleWWDCApplication/AppleWWDCApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppleWWDCApplication
+//
+//  Created by fengzhong on 16/7/28.
+//  Copyright © 2016年 JYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppleWWDCApplication/AppleWWDCApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppleWWDCApplication
+//
+//  Created by fengzhong on 16/7/28.
+//  Copyright © 2016年 JYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
